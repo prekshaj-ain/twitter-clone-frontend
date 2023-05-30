3f0c53d9ba22f470cd36bc1be0914112c94e60cd
preksha <jainpreksha2712@gmail.com>
2023-05-30 16:18:21 +0530

    Basic setup of react app using parcel bundler

---
+++ readme.md
@@ -0,0 +1,2 @@
+## welcome to the twitter clone frontend
+
---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React from "react";
+ 
+/* 
+    - App Layout
+    - Nav
+        - logo
+        - navlinks
+    - body
+        - posts
+            -post
+            -post
+            -post
+
+    - aside section
+        
+*/
+
+const App = ()=>{
+    <h1>Hello</h1>
+}
+
+export default App;
