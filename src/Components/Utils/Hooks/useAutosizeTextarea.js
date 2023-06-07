49124ed8280bc572ead1d7ab6f9b12eeeab8babe
preksha <jainpreksha2712@gmail.com>
2023-06-08 01:08:41 +0530

    added textarea auto resize custom hook

---
+++ src/Components/Utils/Hooks/useAutosizeTextarea.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+
+const useAutosizeTextArea = (textAreaRef, value) => {
+  useEffect(() => {
+    if (textAreaRef) {
+      // We need to reset the height momentarily to get the correct scrollHeight for the textarea
+      textAreaRef.current.style.height = "0px";
+      const scrollHeight = textAreaRef.current.scrollHeight;
+
+      // We then set the height directly, outside of the render loop
+      // Trying to set this with state or a ref will product an incorrect value.
+      textAreaRef.current.style.height = scrollHeight + "px";
+    }
+  }, [textAreaRef, value]);
+};
+
+export default useAutosizeTextArea;
