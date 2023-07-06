77f108e6baae981c0b72ba499d54b4e57c89febb
preksha <jainpreksha2712@gmail.com>
2023-07-06 16:49:33 +0530

    added axios instance for private request(Authorised)

--- src/Components/Utils/instance.js
+++ src/Components/Utils/instance.js
@@ -4,3 +4,11 @@ export const api = axios.create({
   baseURL: "http://localhost:3000/api/v1",
   withCredentials: true,
 });
+
+export const axiosprivate = axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
