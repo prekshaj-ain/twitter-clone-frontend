d9cb4a1978b0036a46f676911fa478c43d10d090
preksha <jainpreksha2712@gmail.com>
2023-07-09 21:07:05 +0530

    setup javascript-time-ago

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
 
 import App from "./Components/app";
 import store from "./Components/Utils/store";
+
+TimeAgo.addDefaultLocale(en);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
