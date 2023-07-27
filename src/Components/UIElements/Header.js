61de837ee2e2d693c4291a03a197b285d98b7d1e
preksha <jainpreksha2712@gmail.com>
2023-07-27 15:35:41 +0530

    Moved header to uielements

---
+++ src/Components/UIElements/Header.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Header = ({ feedTypes, onFeedTypeChange }) => {
+  const [feedType, setFeedType] = useState(feedTypes[0]);
+
+  const clickHandler = (type) => {
+    setFeedType(type);
+    onFeedTypeChange(type);
+  };
+
+  return (
+    <div className="border-b-[1px] sticky top-0 backdrop-blur-xl z-10">
+      <h4 className="p-2 font-bold">Home</h4>
+      <div className="flex">
+        {feedTypes.map((type) => (
+          <div
+            key={type}
+            className="flex-grow flex justify-center hover:bg-gray-200 hover:cursor-pointer"
+            onClick={() => clickHandler(type)}
+          >
+            <div
+              className={`py-3 font-semibold text-gray-500 ${
+                feedType === type && "!text-black border-b-4 border-blue-500"
+              }`}
+            >
+              {type}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
